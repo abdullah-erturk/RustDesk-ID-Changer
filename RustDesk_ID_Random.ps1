0c307616cd395546b74b2f2316331a320a4fa20e
Abdullah ERTÜRK <54866587+abdullah-erturk@users.noreply.github.com>
2023-10-20 16:15:55 +0300

    Add files via upload

---
+++ RustDesk_ID_Random.ps1
@@ -0,0 +1,20 @@
+Stop-Service RustDesk
+
+taskkill /im rustdesk.exe /f
+
+# Rastgele 9 karakter uzunluğunda bir sayı oluştur
+$randomId = -join ((48..57) | Get-Random -Count 9 | ForEach-Object {[char]$_})
+
+$id = Get-Content "C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml" | Select-Object -Index 0
+
+Write-Host "Current ID: $id"
+$newId = "id = '$randomId'"
+Write-Host "New ID: $newId"
+
+$fileContent = Get-Content -Path "C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml"
+
+$newContent = $fileContent -replace [regex]::Escape($id), $newId
+
+$newContent | Set-Content -Path "C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml"
+
+Start-Service RustDesk
